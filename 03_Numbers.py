428632cc72d7c0bf05a211b54b37c14c00d38e26
jfs-coder <jfs-coder@protonmail.com>
2021-01-26 04:26:04 +0000

    Socratica - Number Types

---
+++ 03_Numbers.py
@@ -0,0 +1,28 @@
+# Types of numbers: int, float, complex
+
+# integers (int)
+
+a = 496 # This is a perfect number
+print(type(a))
+print(a)
+
+# floats
+
+e = 2.718281828
+
+print(type(e))
+print(e)
+
+# complex numbers
+
+z = 2 - 6.1j
+
+print(type(z))
+
+print(z)
+
+print(z.real)
+
+print(z.imag)
+
+
