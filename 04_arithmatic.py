4b82e51f84467352d566610ffb0fa74f1c23530a
jfs-coder <jfs-coder@protonmail.com>
2021-01-26 11:26:56 -0500

    starting the arithmatic tutorial

---
+++ 04_arithmatic.py
@@ -0,0 +1,26 @@
+# Numbers: int, float, complex
+# Operations: + - * /
+
+x = 28 # int
+y = 28.0 # float
+
+print(f'converting int 28 to float: {float(28)}') # convert to float
+
+print(f'converting float 3.14 to int: {int(3.14)}') # convert to int
+
+# ints are narrower than floats
+# floats are wider than ints
+
+# simalarly any floats can be made into a complex number, but not vice-versa
+
+tmp = 1.732 + 0j
+
+print(tmp)
+
+print(type(tmp))
+
+print(complex(1.732)) # convert to complex number
+
+
+
+
