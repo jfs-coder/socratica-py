5584198d4d9ced1bdc6231902377bfd1b0c6f8fc
jfs-coder <jfs-coder@protonmail.com>
2021-01-28 04:27:43 +0000

    finished the Socratica tutorial for booleans

--- 05_boolean.py
+++ 05_boolean.py
@@ -14,4 +14,10 @@ print(f'bool(0): {bool(0)}') # prints False
 print(f'bool("Turing"): {bool("Turing")}') # prints True
 print(f'bool(" "): {bool(" ")}') # prints True
 print(f'bool(""): {bool("")}') # prints False
+print(f'str(True): {str(True)}') # prints 'True'
+print(f'str(False): {str(False)}') # prints 'False'
+print(f'int(True): {int(True)}') # prints 1
+print(f'int(False): {int(False)}') # prints 0
+print(f'5 + True: {5 + True}') # prints 6 because True converted to 1 and added to 5
+print(f'10 * False: {10 * False}') # prints 0 because False converted to 0 and then multiplied by 10
 
