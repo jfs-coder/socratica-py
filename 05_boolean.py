235df9cb309ceb9c3b89a8de462459f1b216408a
jfs-coder <jfs-coder@protonmail.com>
2021-01-28 04:23:03 +0000

    working with booleans

---
+++ 05_boolean.py
@@ -0,0 +1,17 @@
+# Boolean values: True, False
+a = 3
+b = 5
+print('a = 3   b = 5')
+print(f'a == b: {a == b}') # prints False
+print(f'a != b: {a != b}') # prints True
+print(f'a > b: {a > b}') # prints False
+print(f'a < b: {a < b}') # prints True
+print(f'type (True): {type(True)}') # prints 'bool'
+print(f'type (False): {type(False)}') # prints 'bool'
+print(f'bool(28): {bool(28)}') # prints True
+print(f'bool(-2.71828): {bool(-2.71828)}') # prints True
+print(f'bool(0): {bool(0)}') # prints False
+print(f'bool("Turing"): {bool("Turing")}') # prints True
+print(f'bool(" "): {bool(" ")}') # prints True
+print(f'bool(""): {bool("")}') # prints False
+
